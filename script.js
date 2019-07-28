163b1ac076bb16e525794375cd91948dfcc036f0
Robert Wise <robcodehub@gmail.com>
2019-07-28 12:55:08 +0200

    prevented default form submit behavior

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-
+//check the js file is linked to the html file
 console.log("linked");
 
-
-
+//create variables for required elements
+const gridmaker = document.querySelector("#gridbutton");
+const gridformvar = document.querySelector("#gridform");
 
 
 //Create render function
@@ -13,3 +14,16 @@ const render = () => {
 
 };
 render();
+
+
+//add an event listener to the button
+
+gridmaker.addEventListener('click', (ev) => {
+
+  console.log("button is working");
+
+});
+
+gridformvar.addEventListener('submit', (ev) => {
+ev.preventDefault();
+});
