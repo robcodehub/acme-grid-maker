937a382c12f120bc381259feec9a16e2de98bf14
Robert Wise <robcodehub@gmail.com>
2019-07-28 22:15:48 +0200

    got one row to change color on and off when clicked

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const box1 = document.querySelector('#box1');
 const box2 = document.querySelector('#box2');
 const gridHolder = document.querySelector("#grid-layout");
 
+
+
 //Create render function
 
 const render = () => {
@@ -34,12 +36,52 @@ function createGrid() {
 console.log("create grid function ran");
 console.log("box1 * box2 = ", Number(box1.value) * Number(box2.value));
 
-let gridhtml = ""
+let gridcolumns = "";
+let gridrows = "";
+
+let squaresArr = [];
 
 for (let i=0;i<box1.value;i++) {
-  gridhtml += "<div class='grid-square'></div>";
+  gridcolumns += "<div class='grid-square'></div>";
+  squaresArr.push({
+    checked: false
+  });
+
 };
 
-gridHolder.innerHTML = gridhtml;
+
+
+
+for (let i=0;i<box2.value;i++) {
+gridrows += "<div class='grid-row-start'>"
+gridrows += gridcolumns;
+gridrows += "</div>"
+}
+
+gridHolder.innerHTML = gridrows;
+
+
+//add event listener to each box when clicked
+const gridSquareVar = document.getElementsByClassName("grid-square");
+
+for(let i=0;i<gridSquareVar.length;i++) {
+gridSquareVar[i].addEventListener('click', (ev) => {
+  console.log("clicked on ", i);
+  squaresArr[i].checked = !squaresArr[i].checked;
+  console.log(squaresArr);
+  if (gridSquareVar[i].className === "grid-square") {
+  gridSquareVar[i].className = "grid-square clicked";
+  } else {
+    gridSquareVar[i].className = "grid-square";
+  }
+});
+}
+
+
+
 
 }
+
+
+
+
