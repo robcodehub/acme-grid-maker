f22a796e69fe41ce5bd02030c9cab4cf02318996
Robert Wise <robcodehub@gmail.com>
2019-07-28 13:27:03 +0200

    multiples box values

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 //check the js file is linked to the html file
-console.log("linked");
+console.log("JS script linked");
 
 //create variables for required elements
 const gridmaker = document.querySelector("#gridbutton");
 const gridformvar = document.querySelector("#gridform");
-
+const box1 = document.querySelector('#box1');
+const box2 = document.querySelector('#box2');
 
 //Create render function
 
@@ -21,9 +22,14 @@ render();
 gridmaker.addEventListener('click', (ev) => {
 
   console.log("button is working");
-
+  createGrid()
 });
 
 gridformvar.addEventListener('submit', (ev) => {
 ev.preventDefault();
 });
+
+function createGrid() {
+console.log("create grid function ran");
+console.log("box1 * box2 = ", Number(box1.value) * Number(box2.value));
+};
