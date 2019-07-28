a2a7f6274e3216ff8822ad18cefb205e5e92eb88
Robert Wise <robcodehub@gmail.com>
2019-07-28 19:38:53 +0200

    one line of divs working

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const gridmaker = document.querySelector("#gridbutton");
 const gridformvar = document.querySelector("#gridform");
 const box1 = document.querySelector('#box1');
 const box2 = document.querySelector('#box2');
+const gridHolder = document.querySelector("#grid-layout");
 
 //Create render function
 
@@ -32,4 +33,13 @@ ev.preventDefault();
 function createGrid() {
 console.log("create grid function ran");
 console.log("box1 * box2 = ", Number(box1.value) * Number(box2.value));
+
+let gridhtml = ""
+
+for (let i=0;i<box1.value;i++) {
+  gridhtml += "<div class='grid-square'></div>";
 };
+
+gridHolder.innerHTML = gridhtml;
+
+}
